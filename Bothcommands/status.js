b60f38bb1a95b3edc1ba116932ca63d040528cb7
Gabe <Gabolicious@users.noreply.github.com>
2020-07-06 16:50:39 -0400

    Updated status to work with database

--- Bothcommands/status.js
+++ Bothcommands/status.js
@@ -3,42 +3,17 @@ const botconfig = require("../botconfig.json");
 const functions = require("../functions.js")
 
 module.exports.run = async (bot, message, args) => {
-  if (message.member.hasPermission("KICK_MEMBERS") && args[0]) { //if they can kick members and provides an ID
-    var ID = args[0]
-  } else if (args[0]) { //can't kick members but provides ID
-    message.channel.send("You can't check someone elses application. Searching for yours instead...")
-  }
-
-  if (message.guild.id == botconfig.PIGSServer) {
-    var SignMeUpIndex = botconfig.PIGSSignMeUpIndex
-  } else if (message.guild.id == botconfig.RTSServer) {
-    var SignMeUpIndex = botconfig.RTSSignMeUpIndex
-  }
-
-  authentication.authenticate().then(async (auth) => {
-    let FoundMember = false; //haven't found member
-    if (!ID) {
-      await functions.FindApplicant(auth, message.channel, message.member.user.tag, botconfig.ApplicationDiscordIndex, SignMeUpIndex, function (row) { //Find member by discord
-        FoundMember = true;
-        if (!row[0]) message.channel.send("We have received your application.") //Nothing in status
-        else if (row[0] == "Hired") message.delete();
-        else message.channel.send(`Your current application status is: ${row[0]}`) //Send status
-      })
-    } else { //Provided ID
-      await functions.FindApplicant(auth, message.channel, ID, botconfig.ApplicationInGameIDIndex, SignMeUpIndex, function (row) { //Find member by ID
-        FoundMember = true;
-        if (!row[0]) message.channel.send("We have received your application.") //Nothing in status
-        else if (row[0] == "Hired") message.delete();
-        else message.channel.send(`Your current application status is: ${row[0]}`) //Send status
-      })
+  con.query(`SELECT * FROM applications WHERE discord_id = '${message.author.id}'`, function (err, result, fields) {
+    if (err) {
+        console.log(err)
+        return;
     }
-  
-    if (!FoundMember) { //Didn't find an application
-      message.channel.send("Couldn't find that applicant")
-      return;
+    if (result.length < 1) {
+       message.channel.send("Unable to find your application.")
+    } else {
+        message.channel.send(`Your application status is: ${result[0].status}`)
     }
-  });
-
+})
 }
 
 module.exports.help = {
