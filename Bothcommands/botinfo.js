c1c526a7ec17d15ee2c8605e4d86e139dd99254e
Gabe Corridore <corridg@rose-hulman.edu>
2021-06-13 18:17:11 -0400

    Formatted numbers

--- Bothcommands/botinfo.js
+++ Bothcommands/botinfo.js
@@ -6,16 +6,15 @@ module.exports.run = async (bot, message, args) => {
     const used = process.memoryUsage().heapUsed / 1024 / 1024; //get used
     const memberused = Math.round(used * 100) / 100 //then round
 
-
     const botembed = new Discord.MessageEmbed()
         .setDescription("***Bot Information***")
         .setColor("#15f153")
         .setThumbnail(bicon)
         .addField("Created By", "Gabo")
         .addField("Created On", bot.user.createdAt)
-        .addField("Memery Used in MB", memberused)
+        .addField("Memery Used in MB", functions.numberWithCommas(memberused))
         .addField("Ping", bot.ws.ping)
-        .addField("Minutes online", Math.round(bot.uptime / 60000)) //Convert to minutes
+        .addField("Minutes online", functions.numberWithCommas(Math.round(bot.uptime / 60000))) //Convert to minutes
         .addField("GitHub", "https://github.com/Gabolicious/AlfredBot")
 
     return message.channel.send(botembed);
